2347ca5135237aee908e432949e7e5e08b3e961b
Joe Malone <joe.d.malone@gmail.com>
2021-03-06 10:47:07 -0600

    activity 22 completed

--- 10-OOP/01-Activities/22-Stu_Classes/Unsolved/character.js
+++ 10-OOP/01-Activities/22-Stu_Classes/Unsolved/character.js
@@ -1,10 +1,24 @@
 class Character {
-  // TODO: Add a constructor
+  constructor(name, strength, hitPoints) {
+    this.name = name;
+    this.strength = strength;
+    this.hitPoints = hitPoints;
+  }
   // TODO: Create a printStats() method that console logs `this.name`, `this.strength`, and `this.hitPoints`
-
+  printStats() {
+    console.log(this.name, "\n", this.strength, "\n", this.hitPoints, "\n");
+  }
   // TODO: Create a isAlive() method that returns a boolean based on whether or not a character's "hitpoints" are <= 0
+  isAlive(){
+    if(this.hitPoints>0) {
+      return(true)
+    } else {false}
+  }
 
   // TODO: Create a attack() method that accepts an opponent object and decreases the opponent's "hitPoints" by this character's strength
+  attack(object) {
+    object.hitPoints -= this.strength;
+  }
 }
 
 // Creates two unique characters using the "character" constructor
